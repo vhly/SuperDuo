1bf771b099d2eb0719ff65bd8a4c3df6f9ff6e5a
Zhang Xiaohe <vhly@163.com>
2015-09-24 00:18:25 +0800

    Update AppWidget show.

--- Football_Scores/app/src/main/java/barqsoft/footballscores/appwidget/MatchAppWidget.java
+++ Football_Scores/app/src/main/java/barqsoft/footballscores/appwidget/MatchAppWidget.java
@@ -2,17 +2,32 @@ package barqsoft.footballscores.appwidget;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.database.Cursor;
 import android.widget.RemoteViews;
+import barqsoft.footballscores.DatabaseContract;
 import barqsoft.footballscores.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class MatchAppWidget extends AppWidgetProvider {
 
+    private static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+    private static String dateStr;
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+        Date currentDate = new Date();
+
+        dateStr = format.format(currentDate);
+
         // There may be multiple widgets active, so update all of them
         final int N = appWidgetIds.length;
         for (int i = 0; i < N; i++) {
@@ -38,6 +53,32 @@ public class MatchAppWidget extends AppWidgetProvider {
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.match_app_widget);
         views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        ContentResolver resolver = context.getContentResolver();
+
+        Cursor cursor = resolver.query(
+                DatabaseContract.scores_table.buildScoreWithDate(), // uri
+                null,  // project
+                null,  // selection
+                new String[]{dateStr},  // selectionArgs
+                null   // orderBy
+        );
+
+        if (cursor != null) {
+
+            if (cursor.moveToNext()) {
+                int columnIndex = cursor.getColumnIndex(DatabaseContract.scores_table.DATE_COL);
+                if (columnIndex != -1) {
+                    String string = cursor.getString(columnIndex);
+                    if (string != null) {
+                        views.setTextViewText(R.id.appwidget_text, string);
+                    }
+                }
+            }
+
+            cursor.close();
+        }
+
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
